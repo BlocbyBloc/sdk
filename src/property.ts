645f930e77fca86020ee4f0b0fde434b0097dca1
nimishsantosh107 <36289353+nimishsantosh107@users.noreply.github.com>
2023-12-04 15:55:26 +0530

    completed propertyFactory class

--- src/property.ts
+++ src/property.ts
@@ -1,9 +1,16 @@
 import { ERC20AssetFactory, ERC20AssetFactory__factory } from "./interfaces";
 import { ethers, BigNumber, BigNumberish, Signer, utils, ContractTransaction } from "ethers";
 
-export interface PropertyFactoryInterface {}
+export interface PropertyFactoryInterface {
+    readonly address: string;
+
+    initAsset: (name: string, symbol: string) => Promise<string>;
+
+    getAssets: () => Promise<string[]>;
+}
 
 export class PropertyFactory implements PropertyFactoryInterface {
+    /* PRIVATE PROPERTIES */
     private readonly _signer: Signer;
     private readonly _contract: ERC20AssetFactory;
 
@@ -12,6 +19,10 @@ export class PropertyFactory implements PropertyFactoryInterface {
         return this._contract.address;
     }
 
+    /**
+     * @param signer Signer to use to deploy market
+     * @param propertyFactoryAddress Address of the deployed ERC20AssetFactory contract
+     */
     constructor(signer: Signer, propertyFactoryAddress: string) {
         this._signer = signer;
         this._contract = ERC20AssetFactory__factory.connect(propertyFactoryAddress, signer);
@@ -20,6 +31,12 @@ export class PropertyFactory implements PropertyFactoryInterface {
         }
     }
 
+    /**
+     * Initializes an asset on the blockchain.
+     * @param name Asset name
+     * @param symbol Asset symbol
+     * @returns Asset contract address
+     */
     initAsset = async (name: string, symbol: string): Promise<string> => {
         const tx = await this._contract.initAsset(name, symbol);
         const tr = await tx.wait(1);
@@ -39,6 +56,15 @@ export class PropertyFactory implements PropertyFactoryInterface {
             console.error(JSON.stringify(tr, null, 2));
             throw new Error("No AssetInitialization Event fired. Please check the TR above");
         }
+    };
+
+    /**
+     * Returns list of all created assets by factory
+     * @returns string[] addresses
+     */
+    getAssets = async (): Promise<string[]> => {
+        const assetAddresses = await this._contract.getAssets();
+        return assetAddresses;
     };
 }
 
